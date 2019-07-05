d81f8ff0972cd7ea25b71701932c3cd55a49c8fd
gamalielvj <gamalielvj@yahoo.com.mx>
2019-07-05 03:51:36 +0000

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,19 +12,6 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
-  # Returns the user corresponding to the remember token cookie.
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-  
   # Forgets a persistent session.
   def forget(user)
     user.forget
@@ -45,9 +32,16 @@ module SessionsHelper
   end
   
   # Returns the current logged-in user (if any).
+  # Returns the user corresponding to the remember token cookie.
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
